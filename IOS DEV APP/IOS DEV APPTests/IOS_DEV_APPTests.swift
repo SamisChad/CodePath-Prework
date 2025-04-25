03f7c98dfea4708b7dd14610d30d8149cb0fb9d0
SamisChad <samyam.paudel.12@gmail.com>
2025-04-25 17:08:39 -0500

    Added prework files to the repository

---
+++ IOS DEV APP/IOS DEV APPTests/IOS_DEV_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_DEV_APPTests.swift
+//  IOS DEV APPTests
+//
+//  Created by Samyam Paudel on 4/25/25.
+//
+
+import Testing
+@testable import IOS_DEV_APP
+
+struct IOS_DEV_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ IOS DEV APP/IOS DEV APPUITests/IOS_DEV_APPUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_DEV_APPUITestsLaunchTests.swift
+//  IOS DEV APPUITests
+//
+//  Created by Samyam Paudel on 4/25/25.
+//
+
+import XCTest
+
+final class IOS_DEV_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
